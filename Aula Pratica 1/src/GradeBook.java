9b32d1aa2772c14ec059a5ad32ac9ea34b8bdb51
MarcielCosta-357 <marciel.357224@alunosatc.edu.br>
2026-05-11 21:35:04 -0300

    Add files via upload

---
+++ Aula Pratica 1/src/GradeBook.java
@@ -0,0 +1,9 @@
+public class GradeBook {
+
+    // O metodo recebe agora um parâmetro String chamado nomeDoCurso
+    public void displayMessage(String nomeDoCurso) {
+        // Exibe a mensagem de forma formatada juntamente com o nome do curso
+        System.out.printf("Welcome to the Grade Book for %s!%n", nomeDoCurso);
+    }
+
+}
