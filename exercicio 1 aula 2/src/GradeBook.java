d778580038910b1209326f59bc82e2b05e0c5be4
MarcielCosta-357 <marciel.357224@alunosatc.edu.br>
2026-05-18 19:43:38 -0300

    Delete exercicio 1 aula 2 directory

--- exercicio 1 aula 2/src/GradeBook.java
+++
@@ -1,9 +0,0 @@
-public class GradeBook {
-
-    // O metodo recebe agora um parâmetro String chamado nomeDoCurso
-    public void displayMessage(String nomeDoCurso) {
-        // Exibe a mensagem de forma formatada juntamente com o nome do curso
-        System.out.printf("Welcome to the Grade Book for %s!%n", nomeDoCurso);
-    }
-
-}
